07a330dcafa94d2cae078d96caf7e23cac2e9aad
MayankSrivastav <mayanksri18@yahoo.in>
2017-02-13 22:17:00 +0530

    inlined critical css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({             
+  grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         uglify: {
@@ -36,14 +36,24 @@ module.exports = function(grunt) {
             src: ['**/*.{png,jpg}'],
             dest: 'views/images/'
           }]
-        }        
-      }      
+        }
+      },
+      critical: {
+        dist: {
+          options: {
+            base: './'
+          },
+          src: 'index.html',
+          dest: 'index.html'
+        }
+      }
   });
 
   // grunt.loadNpmTasks('grunt-contrib-uglify');
   // grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-critical');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.registerTask('default', ['imagemin']);
+  grunt.registerTask('default', ['imagemin', 'critical']);
   // grunt.registerTask('default', ['imagemin', 'responsive_images']);
   // grunt.loadNpmTasks('grunt-responsive-images');
   // grunt.registerTask('default', ['responsive_images']);
